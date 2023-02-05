fabd8c2c53eaaab46aeb5a2d448d9efa6868b48c
Aidan Cruickshank <hapachew@gmail.com>
2023-02-04 21:24:42 -0500

    first version of problem 3 (a1)

--- a1/a1_p3.c
+++ a1/a1_p3.c
@@ -1,11 +1,13 @@
+#include <stddef.h>
 #include <stdio.h> 
+
 #include <stdlib.h> 
 #include <string.h> 
 
 // defining the structure of a matrix
 typedef struct Matrix_s {
     size_t R, C;
-    int *index;
+    int* index;
 } Matrix;
 
 // for printing a line to the screen - mostly to test functions
@@ -25,13 +27,16 @@ int printLine(char* line) {
 }
 
 // printing characteristics of a matrix for checking if its correct
+// Note: this function is inoperable in this problem 
+// (see last problem for its usefulness) 
 int printMatrix(Matrix matrix) { 
-
+    
+    printf("Function currently not working\n"); 
     printf("***New Matrix***\nR: %zu C: %zu\n", matrix.R, matrix.C);
 
     for (size_t row = 0; row < matrix.R; row++) { 
         for (size_t column = 0; column < matrix.C; column++) { 
-            printf("[%zu][%zu] = %d\n", row, column, matrix.index[row][column]); 
+            //printf("[%zu][%zu] = %d\n", row, column, matrix.index[row][column]);
         }
     }
 
@@ -66,7 +71,7 @@ int setRorC(char* line, char* RorC) {
 }
 
 // helper to load a row of a matrix given a line from the input file and its matrix row index
-int loadInnerMatrix(char* line, int indexArray[][], size_t rowIndex) { 
+int loadInnerMatrix(char* line, int* indexArray, size_t rowIndex, size_t columnSize) { 
 
     int column = 0; 
     char* lineCopy = malloc( sizeof(line) ); 
@@ -74,7 +79,7 @@ int loadInnerMatrix(char* line, int indexArray[][], size_t rowIndex) {
     char *num = strtok(line, " ");
 
     while (num != NULL) { 
-        indexArray[rowIndex][column] = atoi(num); 
+        indexArray[(rowIndex * columnSize) + column] = atoi(num); 
         column++; 
         num = strtok(NULL, " "); 
     }
@@ -84,31 +89,37 @@ int loadInnerMatrix(char* line, int indexArray[][], size_t rowIndex) {
 }
 
 // to load the data into a matrix using the above helper methods
+// Note that in this problem the 2D array is implemented as a 1D array 
+// This is because the struct houses a int *index, and not an int **index  
 int loadMatrices(Matrix* matrix1, Matrix* matrix2, FILE* file) { 
 
     size_t lineCounter = 1; 
-    char *line; 
+    int numOfChars = (1000*4) + 999;
+    char *line = malloc( numOfChars * sizeof(char) );
 
     while (fgets(line, numOfChars, file)) { 
         if (lineCounter == 1) { 
             (*matrix1).R = setRorC(line, "R"); 
-            (*matrix1).C = setRorC(line, "C"); 
+            (*matrix1).C = setRorC(line, "C");
+            (*matrix1).index = malloc( ((*matrix1).R * (*matrix1).C) * sizeof(int) ); 
             lineCounter++; 
         } else if (lineCounter > 1 && lineCounter < (*matrix1).R + 2) { 
             size_t rowIndex = lineCounter - 2;
-            loadInnerMatrix(line, (*matrix1).index, rowIndex);
+            loadInnerMatrix(line, (*matrix1).index, rowIndex, (*matrix1).C);
             lineCounter++; 
         } else if (lineCounter == (*matrix1).R + 2) { 
             (*matrix2).R = setRorC(line, "R"); 
-            (*matrix2).C = setRorC(line, "C"); 
+            (*matrix2).C = setRorC(line, "C");
+            (*matrix2).index = malloc( ((*matrix2).R * (*matrix2).C) * sizeof(int)); 
             lineCounter++; 
         } else if (lineCounter > (*matrix1).R + 2) { 
             size_t rowIndex = lineCounter - ((*matrix1).R + 3);
-            loadInnerMatrix(line, (*matrix2).index, rowIndex); 
+            loadInnerMatrix(line, (*matrix2).index, rowIndex, (*matrix2).C); 
             lineCounter++; 
         }
     }
 
+    free(line); 
     return 0; 
 
 }
@@ -128,9 +139,9 @@ int dotProduct(Matrix* matrix1, Matrix* matrix2, Matrix* product) {
         for (size_t counter2 = 0; counter2 < (*matrix2).C; counter2++) { 
             int savedOutput = 0; 
             for (size_t counter3 = 0; counter3 < (*matrix1).C; counter3++) { 
-                 savedOutput += matrix1->index[counter1][counter3] * matrix2->index[counter3][counter2];
+                savedOutput += matrix1->index[(counter1 * matrix1->C) + counter3] * matrix2->index[(counter3 * matrix2->C) + counter2];
             }
-            product->index[counter1][counter2] = savedOutput; 
+            product->index[(counter1 * product->C) + counter2] = savedOutput; 
         }
     }
 
@@ -142,7 +153,13 @@ int dotProduct(Matrix* matrix1, Matrix* matrix2, Matrix* product) {
 int multiplyMatrices(Matrix* matrix1, Matrix* matrix2, Matrix* product){ 
 
     product->R = matrix1->R; 
-    product->C = matrix2->C; 
+    product->C = matrix2->C;
+
+    // now that the product R and C are defined, 
+    // I'll initalize the product index array here
+    product->index = malloc( (product->R * product->C) * sizeof(int) ); 
+    
+    // now we can do the dot product like before
     dotProduct(matrix1, matrix2, product); 
 
     return 0; 
@@ -163,9 +180,9 @@ int main(int argc, char** argv) {
         FILE *outFile = fopen(outFileName, "w");
         
         // building matricies
-        Matrix *matrix1 = malloc( sizeof(Matrix) ); 
-        Matrix *matrix2 = malloc( sizeof(Matrix) ); 
-        Matrix *product = malloc( sizeof(Matrix) ); 
+        Matrix *matrix1; 
+        Matrix *matrix2; 
+        Matrix *product; 
         loadMatrices(matrix1, matrix2, inFile);   
 
         //check if matrices can be multiplied and multiply them
@@ -176,18 +193,15 @@ int main(int argc, char** argv) {
             (*product).C = 0; 
         }
 
-        //remove when running - testing only
-        printMatrix(*product); 
-
         // write the output to the output file
         fprintf(outFile, "%zu %zu", product->R, product->C); 
         for (size_t row = 0; row < product->R; row++) { 
             fprintf(outFile, "\n"); 
             for (size_t column = 0; column < product->C; column++) { 
                 if (column == product->C - 1) { 
-                    fprintf(outFile, "%d", product->index[row][column]);
+                    fprintf(outFile, "%d", product->index[(row * product->C) + column]);
                 } else {
-                    fprintf(outFile, "%d ", product->index[row][column]); 
+                    fprintf(outFile, "%d", product->index[(row * product->C) + column]); 
                 } 
             }
         }
@@ -195,7 +209,9 @@ int main(int argc, char** argv) {
         // close files and free memory
         fclose(inFile); 
         fclose(outFile); 
-        free(outFileName); 
+        free(outFileName);
+        free(matrix1->index); 
+        free(matrix2->index); 
         free(matrix1); 
         free(matrix2); 
         free(product); 
@@ -203,3 +219,4 @@ int main(int argc, char** argv) {
 
     return 0; 
 }
+
